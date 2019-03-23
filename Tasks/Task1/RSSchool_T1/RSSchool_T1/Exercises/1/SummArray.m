6467f23836ae2b4a60801722d72c3d51227d4101
Egor Kozhernovich <piccoloricardo@gmail.com>
2019-03-23 19:20:03 +0300

    First task has been completed

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
@@ -4,7 +4,12 @@
 
 // Complete the summArray function below.
 - (NSNumber *)summArray:(NSArray *)array {
-    return @(0);
+    int res = 0;
+    for (NSNumber *numbFromArray in array) {
+        int val= 0;
+        val = [numbFromArray intValue];
+        res += val;
+        }
+    return [NSNumber numberWithInt:res];
 }
-
 @end
