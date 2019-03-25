0df6f305cca4094713223c6c1769487f1ef3b7a9
Egor Kozhernovich <piccoloricardo@gmail.com>
2019-03-25 23:14:08 +0300

    Third task

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
@@ -4,7 +4,20 @@
 
 // Complete the pangrams function below.
 - (BOOL)pangrams:(NSString *)string {
-    return NO;
+    NSMutableString * string1 =[[NSMutableString alloc] initWithString:[string lowercaseString]];
+
+    NSString *workingString = [string1 stringByReplacingOccurrencesOfString:@" "
+                                                                withString:@""];
+    NSMutableSet *occurrences = [NSMutableSet set];
+    NSMutableString *result = [NSMutableString string];
+    
+    [workingString enumerateSubstringsInRange:NSMakeRange(0, workingString.length) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+        if (![occurrences containsObject:substring]) {
+            [occurrences addObject:substring];
+            [result appendString:substring];
+        }
+    }];
+    return occurrences.count == 26;
 }
 
 @end
