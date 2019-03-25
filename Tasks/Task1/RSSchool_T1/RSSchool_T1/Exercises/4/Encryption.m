20c88fe5c360ac56caff7727ff74b012c2726326
Egor Kozhernovich <piccoloricardo@gmail.com>
2019-03-25 23:55:31 +0300

    Fourth task

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
@@ -4,7 +4,26 @@
 
 // Complete the encryption function below.
 - (NSString *)encryption:(NSString *)string {
-    return @"";
+    double L_sqrt = sqrt(string.length);
+    int ceil_rows = ceil(L_sqrt);
+    int floor_columns = floor(L_sqrt);
+    if ( string.length > ceil_rows * floor_columns ){
+        floor_columns += 1;
+    }
+    NSMutableArray *arrayOfencrypted = [NSMutableArray new];
+    for (int i = 0; i < ceil_rows; i++) {
+        NSMutableString *encryptedMessage = [[NSMutableString alloc] initWithCapacity:floor_columns];
+        for (int column = 0; column < floor_columns; column++) {
+            int pos = ceil_rows * column + i;
+            if (pos < string.length) {
+                NSRange range = NSMakeRange(pos, 1);
+                NSString *symbol = [string substringWithRange:range];
+                [encryptedMessage appendString:symbol];
+            }
+        }
+        [arrayOfencrypted addObject:encryptedMessage];
+    }
+    return [[NSString alloc] initWithString:[arrayOfencrypted componentsJoinedByString:@" "]];
 }
 
 @end
